99c6dd6583a5d3cacb8b4e3af05a59bf6a265035
imqhyy <huydang6726@gmail.com>
2025-11-03 00:14:07 +0700

    update

--- cuahangdienthoai/src/com/model/connguoi.java
+++ cuahangdienthoai/src/com/model/connguoi.java
@@ -1,5 +1,31 @@
 package com.model;
+import java.util.Scanner;
 
-public class ConNguoi {
-
+abstract public class ConNguoi {
+    String hovaten;
+    int tuoi;
+    String sdt;
+    public ConNguoi() {
+        hovaten = "null";
+        tuoi = 0;
+        sdt = "null";        
+    }
+    public ConNguoi(String hovaten, int tuoi, String sdt) {
+        this.hovaten = hovaten;
+        this.tuoi = tuoi;
+        this.sdt = sdt;
+    }
+    private static final Scanner sc = new Scanner(System.in);
+    public void getInfo() {
+        System.out.println("ho va ten: " + hovaten);
+        System.out.println("tuoi: " + tuoi);
+        System.out.println("so dien thoai: " + sdt);
+    }
+    public void setInfo() {
+        System.out.print("ho va ten: ");
+        hovaten = sc.nextLine();
+        tuoi = sc.nextInt();
+        sc.nextLine(); //doc ky tu enter
+        sdt = sc.nextLine();
+    }
 }
--- cuahangdienthoai/src/com/model/khachhang.java
+++ cuahangdienthoai/src/com/model/khachhang.java
@@ -1,5 +1,15 @@
 package com.model;
 
-public class KhachHang {
+public class KhachHang extends ConNguoi {
+    String ID;
+    public KhachHang() {
+        super();
+        ID = "null";
+    }
+    public KhachHang(String hovaten, int tuoi, String sdt, String ID) {
+        super(hovaten, tuoi, sdt);
+        this.ID = ID;
+    }
+    
 
 }
